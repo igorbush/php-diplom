17a1807a2edcd3eb2ff34103b99a20d36857216e
Igor Bushenev <igorbushenev@mail.ru>
2018-02-11 19:23:25 +0300

    fix db data

--- config/dbdata.php
+++ config/dbdata.php
@@ -1,8 +1,8 @@
 <?php return [
 	'mysql' => [
-		'host' => 'localhost',
-		'dbname' => 'diplom',
-		'user' => 'mysql',
-		'pass' => 'mysql',
+		'host' => 'tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		'dbname' => 'h9cytpipn719zrp8',
+		'user' => 'ax8nhn3dpvr1or9i',
+		'pass' => 'wobcetwmlubn0n3p',
 	]
 ];
