302209d1d5e3bf76a7efae3e631ae7c877da547b
igorbush <igorbushenev@mail.ru>
2018-02-11 19:03:26 +0300

    Update dbdata.php

--- config/dbdata.php
+++ config/dbdata.php
@@ -1,8 +1,8 @@
 <?php return [
 	'mysql' => [
-		'host' => 'localhost',
-		'dbname' => 'diplom',
-		'user' => 'mysql',
-		'pass' => 'mysql',
+		'host' => 'tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		'dbname' => 'mk5tzaedii9mkydo',
+		'user' => 'azya2tt78lr1pfda',
+		'pass' => 'afy3au1n4kblyfw6',
 	]
 ];
