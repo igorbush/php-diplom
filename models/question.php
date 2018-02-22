b23fa555a61cc3e461b1fcf0b9b08638e92e71f4
Igor Bushenev <igorbushenev@mail.ru>
2018-02-22 22:31:01 +0300

    elegram test #6

--- models/question.php
+++ models/question.php
@@ -89,6 +89,20 @@ class Question
 
 	/**
 	* @param int $id
+	* @return array
+	*/
+
+	public function getForTelegram($id) 
+	{
+		$query = "SELECT chat_id FROM questions WHERE id = ?";
+		$sth = $this->db->prepare($query);
+		$sth->bindValue(1, $id, PDO::PARAM_INT);
+		$sth->execute();
+		return $sth->fetch(PDO::FETCH_ASSOC);
+	}
+
+	/**
+	* @param int $id
 	* @param string $action
 	* @param string $session_user
 	* @return boolean
